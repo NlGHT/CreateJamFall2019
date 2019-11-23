a72efd7538538dff079ed7698be7a86809fbc18a
PolleAnker <thementoryay@gmail.com>
2019-11-23 11:56:45 +0100

    Early power-up push
    Power-ups are spawned on death of enemy object - doesn't do anything yet

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int powerUpType;
+
+    void Start()
+    {
+        setType();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime*50, 0));
+    }
+
+    private int setType()
+    {
+        powerUpType = Random.Range(1, 6);
+        print(powerUpType);
+
+        return powerUpType;
+    }
+
+    private void part()
+    {
+        // On collision this should increase strength of the character which picks it up
+    } 
+}
