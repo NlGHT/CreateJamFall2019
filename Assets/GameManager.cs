6e70f8f637f8dbb0484e642eb49f092ed1e18b27
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2019-11-23 20:27:51 +0100

    Crate and beginning of GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    public GameObject SpikerPrefab;
+    public GameObject SploderPrefab;
+
+    public List<GameObject> spawnPoints;
+
+    public int numEnemiesLeft;
+
+    public GameObject gate;
+    public GameObject finishLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
