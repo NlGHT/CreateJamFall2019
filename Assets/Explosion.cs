4985162ee97924a89fdf5eb4e006013906d069f3
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2019-11-23 12:44:24 +0100

    Explosion!

--- Assets/Explosion.cs
+++ Assets/Explosion.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+
+    bool hasExploded = false;
+    public float force = 1;
+    public float radius = 1;
+    
+    public List<Rigidbody> collidingBodies;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +20,22 @@ public class Explosion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasExploded)
+        {
+            hasExploded = true;
+            for (int i = 0; i < collidingBodies.Count; i++)
+            {
+                //Vector3 dir = collidingBodies[i].transform.position - gameObject.transform.position;
+                collidingBodies[i].AddExplosionForce(force, transform.position, radius);
+            }
+        }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        collidingBodies.Add(other.gameObject.GetComponent<Rigidbody>());
     }
+
 
 }
