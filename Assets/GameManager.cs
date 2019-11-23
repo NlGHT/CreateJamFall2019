c6383f3faee2fa3fb69251a2690294aba835dfbb
PeterBusk <36533756+PeterBusk@users.noreply.github.com>
2019-11-23 20:36:05 +0100

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
+    
 
     public GameObject SpikerPrefab;
     public GameObject SploderPrefab;
@@ -12,19 +13,53 @@ public class GameManager : MonoBehaviour
     public List<GameObject> spawnPoints;
 
     public int numEnemiesLeft;
+    public int amountSpawnedPerWave;
 
     public GameObject gate;
     public GameObject finishLine;
 
+    private FinishLine fl;
+
+
+    public float timer;
+    public float spawnInterval;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = spawnInterval;
+        fl = finishLine.GetComponent<FinishLine>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        timer -= Time.deltaTime;
+
+        if (fl.getPlayerCount() == 2)
+        {
+            nextScene();
+        }
+
+
+        if (timer < 0)
+        {
+            spawnInterval -= (spawnInterval / 5);
+            timer = spawnInterval;
+
+
+
+
+
+        }
+
     }
+
+
+    private void nextScene()
+    {
+
+    }
+
 }
